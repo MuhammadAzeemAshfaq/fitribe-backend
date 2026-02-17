f9133fa39a85ccc70b4325bb76e2f6603bd7b9e2
Bilal Tariq <billal.tariq03@gmail.com>
2026-02-17 23:41:20 +0500

    fixed index.js to use env variables

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const swaggerSpecs = require('./config/swagger');
 const { requestLogger } = require('./utils/logger');
 
 // Initialize Firebase Admin
-const serviceAccount = require('./serviceAccountKey.json');
+// const serviceAccount = require('./serviceAccountKey.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const app = express();
